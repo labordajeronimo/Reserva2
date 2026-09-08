ede9e0eccf85fb0fcecdc17865e9c5700ee5d96d
labordajeronimo <labordajeronimo1@gmail.com>
2026-09-08 14:12:25 -0300

    Refactor de Landing Page: nuevo Navbar, Hero limpio, Footer claro y CSS optimizado

--- Frontend/reserva2-app/src/app/app.ts
+++ Frontend/reserva2-app/src/app/app.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
-  styleUrls: ['./app.css'] 
+  styleUrls: ['./app.css']
 })
 export class App {
   title = 'reserva2-app';
+  isScrolled = false; // Variable que nos dice si bajamos
+
+  // Esta función detecta el scroll
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 50;
+  }
 }
